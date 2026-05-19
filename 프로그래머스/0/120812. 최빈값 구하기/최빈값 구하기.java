2be8ab663ab6d40f05dddeec7cc0fae815dbb0ce
monkey <105265603+Develop-KIM@users.noreply.github.com>
2026-05-19 14:26:09 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.05 ms, Memory: 60.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int[] array) {
+
+        int[] count = new int[1000];
+
+        for (int number : array) {
+            count[number]++;
+        }
+
+        int max = 0;
+        int answer = -1;
+
+        for (int i = 0; i < count.length; i++) {
+
+            if (count[i] > max) {
+                max = count[i];
+                answer = i;
+            } else if (count[i] == max && max != 0) {
+                answer = -1;
+            }
+        }
+
+        return answer;
+    }
+}
